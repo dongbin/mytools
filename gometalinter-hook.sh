02f4fb6f31a9f87c245f63a0d234bdd555695756
Dong Bin <dongbin1@guazi.com>
2017-07-27 17:53:38 +0800

    add hook sh

--- gometalinter-hook.sh
+++ gometalinter-hook.sh
@@ -1,4 +1,4 @@
 go get -u github.com/alecthomas/gometalinter
 gometalinter --install
-echo gometalinter ./... --config=gometalinter.json > .git/hooks/pre-commit
+echo ./validate.sh > .git/hooks/pre-commit
 chmod +x .git/hooks/pre-commit
